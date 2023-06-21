74dd960e4172cae01d03b8a6518e35ef94100480
KalistusAlvino <kalistus33@gmail.com>
2023-06-21 18:19:06 +0700

    UAS NO 5 72210487

---
+++ app/Http/Controllers/uasController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class uasController extends Controller
+{
+    public function uas5(){
+        date_default_timezone_set("Asia/Bangkok");
+
+        $time = date('H:i:s');
+
+        if($time >='19:00' && $time <= '02:59'){
+            $echo = 'Selamat malam';
+            return view('uas5',['echo'=>$echo]);
+        }
+
+        else if($time >='03:00' && $time <= '09:59'){
+            $echo = 'Selamat Pagi';
+            return view('uas5',['echo'=>$echo]);
+        }
+
+        else if($time >='10:00' && $time <= '14:59'){
+            $echo = 'Selamat Siang';
+            return view('uas5',['echo'=>$echo]);
+        }
+
+        else if($time >='15:00' && $time <= '17:59'){
+            $echo = 'Selamat Sore';
+            return view('uas5',['echo'=>$echo]);
+        }
+        else if($time >='18:00' && $time <= '18:59'){
+            $echo = 'Selamat Petang';
+            return view('uas5',['echo'=>$echo]);
+        }
+    }
+}
